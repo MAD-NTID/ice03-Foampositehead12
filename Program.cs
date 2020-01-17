a14d0e3334b3b844be384b78f54507f41c72544f
Asido <asido@asidos-mbp.wireless.rit.edu>
2020-01-17 15:40:23 -0500

    ICE03 another one

--- Program.cs
+++ Program.cs
@@ -3,26 +3,26 @@
 namespace ICE03
 //Asido Yellow-Duke
 //ICE03
-//August 30,2019
+//January 17,2019
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int age = 27;
+            int myage = 27;
             int year = 1992;
             string highSchool = "Murry Bergtraum High School";
             int graduatedYear = 2012;
             string myMomName = "Grace";
             string myDadName = "Effiom";
-            int credit = 17;
-            string program = "Program Fund";
+            int credit = 13;
+            string programFund = "Program Fund";
             string myFirstName = "Asido";
             string myLastName = "Yellow-Duke"; 
 
-            Console.WriteLine("Hola, My name is "+ myFirstName + " "+ myLastName +".I'm "+age+"years old.\n"); 
+            Console.WriteLine("Hola, My name is "+ myFirstName + " "+ myLastName +".I'm "+myage+"years old.\n"); 
             Console.WriteLine("I went to "+ highSchool +" and graduated in "+ graduatedYear +".\nAfter that, i decided to come to NTID for college. Both my Mom "+myMomName +" and Dad "+ myDadName +" "+"though NTID would be great choice for me.");
-            Console.WriteLine("\nSo, now I'm a student here at RIT/NTID. I'm in the "+ program +", taking "+ credit +" credits"+"."+" That's pretty crazy for someone who was born in "+year+".\n");
+            Console.WriteLine("\nSo, now I'm a student here at RIT/NTID. I'm in the "+ programFund +", taking "+ credit +" credits"+"."+" That's pretty crazy for someone who was born in "+year+".\n");
             
         }
     }
